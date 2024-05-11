5f5eba17c61e10da29134e2d22f197d48ef3efcf
Nikesh saini <86639484+nikeshSaini@users.noreply.github.com>
2024-05-11 23:36:18 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const userRouter = express.Router();
-const verifyToken=require("../middleware/auth")
+const verifyToken = require("../middleware/auth")
 const {
   handleGetLogin,
   handleUserSignup,
   getUserProfile,
-  
+
 } = require("../controllers/usercontroller");
 
 
-userRouter.get("/",handleGetLogin)
-userRouter.post("/",handleUserSignup)
-         
-          //its working as goood
-userRouter.get("/get-user-details",verifyToken,getUserProfile)
-        
+userRouter.post("/login", handleGetLogin)
+userRouter.post("/signup", handleUserSignup)
+
+//its working as goood
+userRouter.get("/get-user-details", verifyToken, getUserProfile)
+
 
 
 module.exports = userRouter;
